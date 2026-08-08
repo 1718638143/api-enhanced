bc16ede87b0f6fdb5a37c73a695885c3f845c071
1718638143 <1718638143@qq.com>
2026-08-08 21:45:54 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-// app.js
+#!/usr/bin/env node
 const fs = require('fs')
 const path = require('path')
 const tmpPath = require('os').tmpdir()
 
-// 确保临时目录存在
-if (!fs.existsSync(path.resolve(tmpPath, 'anonymous_token'))) {
-  fs.writeFileSync(path.resolve(tmpPath, 'anonymous_token'), '', 'utf-8')
+async function start() {
+  // 检测是否存在 anonymous_token 文件,没有则生成
+  if (!fs.existsSync(path.resolve(tmpPath, 'anonymous_token'))) {
+    fs.writeFileSync(path.resolve(tmpPath, 'anonymous_token'), '', 'utf-8')
+  }
+  // 启动时更新anonymous_token
+  const generateConfig = require('./generateConfig')
+  await generateConfig()
+  require('./server').serveNcmApi({
+    checkVersion: true,
+  })
 }
-
-// 引入 generateConfig 并执行（如果需要）
-// const generateConfig = require('./generateConfig')
-// generateConfig() 
-
-// 导出服务供 Vercel 调用
-module.exports = require('./server').serveNcmApi({
-  checkVersion: true,
-})
+start()
